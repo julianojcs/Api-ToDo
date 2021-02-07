e3fb2331d8c9110cb9d65dcee62fb952a22b177c
Juliano Costa Silva <apfjuliano@gmail.com>
2021-02-07 00:55:52 -0300

    Middleware de validação

--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const TaskController = require('../controller/TaskController')
+const TaskValidation = require('../middlewares/TaskValidation')
 
 //! Passa apenas '/', pois o '/task' já está sendo 
 //! informado na chamada da rota no src/index.js
-router.post('/', TaskController.create)
+router.post('/', TaskValidation, TaskController.create)
 
 module.exports = router
