bdad87dd2807d7bbca98af31089a7b68adf77448
Juliano Costa Silva <apfjuliano@gmail.com>
2021-02-09 00:40:51 -0300

    Get All Tasks & Validation

--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -3,10 +3,12 @@ const router = express.Router()
 
 const TaskController = require('../controller/TaskController')
 const TaskValidation = require('../middlewares/TaskValidation')
+const MacAddressValidation = require('../middlewares/MacAddressValidation')
 
 //! Passa apenas '/', pois o '/task' já está sendo 
 //! informado na chamada da rota no src/index.js
 router.post('/', TaskValidation, TaskController.create)
-router.put('/:id', TaskController.update)
+router.put('/:id', TaskValidation, TaskController.update)
+router.get('/', MacAddressValidation, TaskController.all)
 
 module.exports = router
